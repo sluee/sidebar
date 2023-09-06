5c4b72341d914ad0a29a2bfd4f50efb8e989e400
Eula Gabas <eulagabas28@gmail.com>
2023-09-06 08:04:35 +0800

    sidebar

---
+++ resources/js/Components/MainNav.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+
+<div class="main-nav">
+    <Link href="#">
+        <i class="fa fa-home"></i> Home</Link>
+    <Link href="#">
+        <i class="fa fa-question"></i> About Us</Link>
+    <Link href="#">
+        <i class="fa-solid fa-cart-shopping"></i> Products</Link>
+    <Link href="#">
+        <i class="fa-regular fa-images"></i> Gallery</Link>
+    <Link href="#">
+        <i class="fa-solid fa-phone-volume"></i> Contact Us</Link>
+</div>
+
+</template>
---
+++ resources/js/Layouts/Layout1.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import MainNav from '@/Components/MainNav.vue'
+    import { ref } from 'vue';
+
+    let width = ref('w-[250px]')
+    let hidden = ref("")
+
+    function toggleWidth() {
+        if(width.value == 'w-[250px]') {
+            width.value = 'w-[50px]'
+            hidden.value = "hidden"
+        }else {
+            width.value = ['w-[250px]']
+            hidden.value = ""
+        }
+    }
+</script>
+
+<template>
+
+<div class="flex min-h-screen">
+    <div id="sidebar" class="bg-gray-100 p-6 duration-500" :class="width" style="position: relative;">
+        <button class="text-xl text-tahiti" @click="toggleWidth" style="position: absolute; right: 10px; top:10px">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <div id="branding" :class="hidden">
+            <img src="https://static.vecteezy.com/system/resources/previews/009/022/996/original/rqr-logo-rqr-letter-rqr-letter-logo-design-initials-rqr-logo-linked-with-circle-and-uppercase-monogram-logo-rqr-typography-for-technology-business-and-real-estate-brand-vector.jpg" alt="Logo"
+                class="w-[170px] h-[170px] mx-auto rounded-full object-cover">
+            <h1 class="text-3xl text-center text-tahiti my-6">RQR Internet services</h1>
+
+        </div>
+        <hr class="border-gray-600">
+
+        <MainNav></MainNav>
+    </div>
+    <div id="container" class="flex-1 ">
+        <slot />
+    </div>
+</div>
+
+</template>
